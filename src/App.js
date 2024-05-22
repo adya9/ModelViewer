6853874156b6c5eb16fc9d29f5d14c3cf2118293
adya <adyakumari2001@gmail.com>
2024-05-22 14:12:29 +0530

    added the delete functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 import LabelComp from './component/LabelComp';
+import {v4 as uuidv4} from 'uuid';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   const [hotspots, setHotspots] = useState([]);
   const [clickPosition, setClickPosition] = useState({ x: null, y: null });
   const [threeDPosition, setThreeDPosition] = useState(null);
+  const [currentHotspot, setCurrentHotspot] = useState(null);
   const modelViewerRef = useRef(null);
 
 
@@ -17,6 +19,7 @@ function App() {
     setSelectedModel(event.target.value);
     setClicked(false);
     setHotspots([]);
+    setCurrentHotspot(null);
   };
 
 
@@ -44,23 +47,67 @@ function App() {
 
 
       const newHotspot = {
-        position: position.toString(),
-        normal: normal.toString(),
+        id:uuidv4(),
+        // position: position.toString(),
+        // normal: normal.toString(),
+
+        position: `${position.x} ${position.y} ${position.z}`,
+        normal: `${normal.x} ${normal.y} ${normal.z}`,
+        type:"question",
         annotation: ''
       };
-
+      
+      const newHotspotJSON = JSON.stringify(newHotspot);
+      console.log(newHotspotJSON);
 
       setHotspots([...hotspots, newHotspot]);
       setThreeDPosition({ position: position.toString(), normal: normal.toString() });
       setClicked(false);
+      setCurrentHotspot(hotspots.length);
     }
   };
 
-
-  const handleLabelChange = (index, newLabel) => {
-    const updatedHotspots = [...hotspots];
-    updatedHotspots[index].annotation = newLabel;
+  const handleLabelChange = (id, newLabel) => {
+    const updatedHotspots = hotspots.map(hotspot =>
+      hotspot.id === id ? { ...hotspot, annotation: newLabel } : hotspot
+    );
     setHotspots(updatedHotspots);
+  };
+
+  const handleLabelDelete = (id) => {
+    const indexToDelete = hotspots.findIndex(hotspot => hotspot.id === id);
+    const updatedHotspots = hotspots.filter(hotspot => hotspot.id !== id);
+    setHotspots(updatedHotspots);
+
+    if (indexToDelete === currentHotspot) {
+     
+      if (updatedHotspots.length === 0) {
+        setCurrentHotspot(null);
+      } else if (indexToDelete === updatedHotspots.length) {
+       
+        setCurrentHotspot(updatedHotspots.length - 1);
+      } else {
+       
+        setCurrentHotspot(indexToDelete);
+      }
+    } else if (indexToDelete < currentHotspot) {
+      
+      setCurrentHotspot(currentHotspot - 1);
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (currentHotspot !== null && currentHotspot > 0) {
+      setCurrentHotspot(currentHotspot - 1);
+      updateCameraOrbit(currentHotspot - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (currentHotspot !== null && currentHotspot < hotspots.length - 1) {
+      setCurrentHotspot(currentHotspot + 1);
+      updateCameraOrbit(currentHotspot + 1);
+    }
   };
 
   useEffect(() => {
@@ -74,6 +121,30 @@ function App() {
       }
     };
   }, [clicked, hotspots]);
+ 
+
+  //used the cartesian coordinates and get their sperical coordinates and pass to camera orbit 
+  const updateCameraOrbit = (index) => {
+    const modelViewer = modelViewerRef.current;
+    if (modelViewer && index !== null && index < hotspots.length) {
+      const hotspot = hotspots[index];
+      const position = hotspot.position.split(' ').map(Number);
+      console.log("Position is"+ position);
+
+      if (position.length === 3) {
+        const [x, y, z] = position;
+
+        const radius = Math.sqrt(x * x + y * y + z * z);
+        const theta = Math.atan2(y, x);
+        const phi = Math.acos(z / radius);
+
+        const newCameraOrbit = `${theta}rad ${phi}rad ${radius}m`;
+        modelViewer.cameraOrbit = newCameraOrbit;
+        console.log("New orbit is "+newCameraOrbit);
+       
+      }
+    }
+  };
 
 
   return (
@@ -101,29 +172,46 @@ function App() {
           ar-modes="webxr"
           camera-controls
           touch-action="pan-y"
+          // camera-orbit="0.2940971513368917rad 6.4386257620807659rad 4.287888690270461m"
+          //camera-orbit="calc(-1.5rad + env(window-scroll-y) * 4rad) calc(0deg + env(window-scroll-y) * 180deg) calc(5m - env(window-scroll-y) * 10m)"
+          //camera-orbit="3.2940971513368917rad 1.4386257620807659rad 4.287888690270461m" 
+          // camera-target={threeDPosition.position}
           src={`${selectedModel}.glb`}
           poster="poster.webp"
           shadow-intensity="1"
-          alt="A 3D model of an astronaut."
+         
           style={{ width: '100%', height: '100%' }}
           ref={modelViewerRef}
         >
-          {hotspots.map((hotspot, index) => (
-            <button
-              key={index}
-              className="hotspot"
-              slot={`hotspot-${index}`}
+          
+          {hotspots.map((hotspot,index) => {
+            console.log(hotspot)
+            return <button
+              key={hotspot.id}
+              className={`hotspot ${index === currentHotspot ? 'highlight' : ''}`}
+              slot={`hotspot-${hotspot.id}`}
               data-position={hotspot.position}
               data-normal={hotspot.normal}
+              
             >
+
               {hotspot.annotation && (
                 <div className="annotation">{hotspot.annotation}</div>
               )
 
               }
             </button>
-          ))}
+            
+          }
+
+          )}
         </model-viewer>
+        <div className="absolute bottom-4 left-10 z-10">
+          <button className="py-2 px-5 text-white bg-orange-600 rounded-full hover:bg-orange-500" onClick={handlePrevClick}>PREV</button>
+        </div>
+        <div className="absolute bottom-4 right-10 z-10">
+          <button className="py-2 px-5 text-white bg-orange-600 rounded-full hover:bg-orange-500" onClick={handleNextClick}>NEXT</button>
+        </div>
       </div>
 
 
@@ -138,12 +226,14 @@ function App() {
         )}
 
         {threeDPosition && (
-          hotspots.map((hotspot, index) => (
+          hotspots.map((hotspot) => (
             <LabelComp
-              index={index}
+              key={hotspot.id}
+              id={hotspot.id}
               label={hotspot.annotation}
               onLabelChange={handleLabelChange}
-              
+              onDelete={handleLabelDelete}
+
             />
           ))
         )}
--- src/component/LabelComp.js
+++ src/component/LabelComp.js
@@ -2,16 +2,21 @@
 import React from 'react';
 
 
-const LabelComp = ({ index, label, onLabelChange}) => {
+const LabelComp = ({ id, label, onLabelChange,onDelete}) => {
   const handleChange = (event) => {
-    onLabelChange(index, event.target.value);
+    onLabelChange(id, event.target.value);
   };
+
+
+  const handleLabelDelete=()=>{
+    onDelete(id);
+  }
 
   return (
     <div className="m-4 p-3 bg-white rounded-md shadow-md ">
       <div className="flex flex-row justify-between">
         <label className="font-semibold">Label </label>
-        <i className="fi fi-rr-trash text-red-500"></i>
+        <i className="fi fi-rr-trash text-red-500" onClick={handleLabelDelete}></i>
         
       </div> 
       <input
